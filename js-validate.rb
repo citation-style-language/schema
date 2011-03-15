b016bd75cbe2fa9c7b58002ccde51b2b577331c3
Bruce D'Arcus <bdarcus@gmail.com>
2011-03-15 18:51:18 -0400

    adding little validator script for json input files

---
+++ js-validate.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'json-schema'
+
+# script takes a pattern of input; for example '/home/json/*.json'
+jsfiles = Dir.glob(ARGV[0])
+schema = 'csl-data.json'
+
+jsfiles.each do |jsfile|
+  begin
+    JSON::Validator.validate!(schema, jsfile)
+  rescue JSON::Schema::ValidationError
+    puts $!.message
+  end
+end
+
