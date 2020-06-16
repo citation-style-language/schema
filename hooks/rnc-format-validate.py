f5c1840c1dc9e9eaa9b82c8a959184985436025f
Bruce D'Arcus <bdarcus@gmail.com>
2020-06-16 13:25:45 -0400

    removed unneeded import statement, comment

--- hooks/rnc-format-validate.py
+++ hooks/rnc-format-validate.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import subprocess
-from pathlib import Path
 
 
 def main():
@@ -8,7 +7,6 @@ def main():
     cmd = ["git", "diff", "--name-only", "--cached"]
     changed_files = subprocess.check_output(cmd, text=True).splitlines()
 
-    # I don't understand why, but this currently isn't working
     for rncfile in changed_files:
         if rncfile.endswith(".rnc"):
             rngfile = rncfile + ".rng"
