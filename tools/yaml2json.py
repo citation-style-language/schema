68d7e8e51b2b000c3d79ae73add59b1060d0b331
Bruce D'Arcus <bdarcus@gmail.com>
2020-07-18 18:32:49 -0400

    indent json

--- tools/yaml2json.py
+++ tools/yaml2json.py
@@ -16,5 +16,4 @@ for yfile in yfiles:
     yaml = ruamel.yaml.YAML(typ='safe')
     data = yaml.load(inf)
     with open(ofn, "w") as write_file:
-        json.dump(data, write_file)
-    #json.dump(data, ofn, indent=2)
+        json.dump(data, write_file, indent=2)
