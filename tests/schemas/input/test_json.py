adc02bbf74c3d78a948b01eb034ac009529c2ae9
Bruce D'Arcus <bdarcus@gmail.com>
2020-08-05 11:26:59 -0400

    update test to match schema

--- tests/schemas/input/test_json.py
+++ tests/schemas/input/test_json.py
@@ -121,35 +121,12 @@ def test_citation_schema_compound_locator_validates(csl_citation_validator):
             "title": "test title"
           },
           "locators": [
-            {"page": [1, 3]},
-            {"line": 12}
+            {"page": 1, "line": 12}
           ]
         }
       ]
     }
     csl_citation_validator.validate(cite)
-
-def test_citation_schema_locator_recursive_array_fails(csl_citation_validator):
-    cite = {
-      "schema": "https://resource.citationstyles.org/schema/latest/input/json/csl-citation.json",
-      "citationID": "item1",
-      "citationItems": [
-          {
-            "id": "item1",
-            "itemData": {
-            "id": "item1",
-            "type": "book",
-            "title": "test title"
-          },
-          "locator": {
-            "page": [1, [3, [4]]]
-          }
-        }
-      ]
-    }
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        csl_citation_validator.validate(cite)
-
 
 def test_basic_data_schema_with_author_validates(csl_data_validator):
     csl = [{
